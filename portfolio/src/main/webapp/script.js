6b3646c700814ad038bc594ee54a0104762c4777
SusanaPerezJ <sperezjuar1@miners.utep.edu>
2020-06-04 09:44:21 +0000

    Add addRandomFact function

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,17 +12,17 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+
 /**
- * Adds a random greeting to the page.
- */
-function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+ * Add a random photo to the page.
+ **/
+ function addRandomFact() {
+     const facts = ['I am bilingual', 'I love plants', 'I have purple hair', 'My favorite color is blue'];
+     //Choose a random fact
+     const randomFact = facts[Math.floor(Math.random() * facts.length)];
+     
+     //Add random fact to the page
+     const factContainer = document.getElementById('fact-container');
+     factContainer.innerText = randomFact;
+ }
 
-  // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
-
-  // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
-}
