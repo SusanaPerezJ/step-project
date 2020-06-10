f81014d64350843d2e5f37cbd414afe36663d997
SusanaPerezJ <sperezjuar1@miners.utep.edu>
2020-06-10 16:16:20 +0000

    Modify doGet() method to return comments
    Add doPost() method to populate ArrayList
    
    Add getParameter() to return the request parameter
    
    Add validComment() to only add comments that are not empty

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -19,16 +19,62 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.*;
+import com.google.gson.Gson;
 
-/** Servlet that returns some example content. TODO: modify this file to handle comments data */
+/** Servlet that returns thread of comments. */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
-
+  public ArrayList<String> comments = new ArrayList<>();
+ 
   @Override 
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    String message = "Hello Susana!";
-    
-    response.setContentType("text/html;");
-    response.getWriter().println(message);
+    response.setContentType("application/json");
+    String json = new Gson().toJson(comments);
+    response.getWriter().println(json);
+  }
+
+ /**
+  * Takes data from the request and populates arraylist
+  */
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    String text = getParameter(request, "comment-box", "");
+    //Handles empty comments, does not add them to the thread
+    if(validComment(text)){
+        comments.add(text);
+        response.setContentType("text/html");
+    }
+
+    // Redirect back to the HTML page.
+    response.sendRedirect("/index.html");
+  }
+/**
+ * Returns the request parameter, or the default value if the parameter was not specified by the client
+ */
+  private String getParameter(HttpServletRequest request, String name, String defaultValue) {
+    String value = request.getParameter(name);
+    if (value == null) {
+      return defaultValue;
+    }
+    return value;
+  }
+/**
+ * Validates a comment if it has at least a character that is not a white space
+ */
+  public boolean validComment(String comment){
+    if(comment.isEmpty()){
+        return false;
+      }
+    for(int j = 0; j < comment.length(); j++){
+        if(comment.charAt(j) != ' '){
+          return true;
+        }else{
+          if(j == comment.length()-1){ //invalid comment if the string is only white spaces
+          return false;
+          }
+        }
+      }
+    return false;
   }
 }
